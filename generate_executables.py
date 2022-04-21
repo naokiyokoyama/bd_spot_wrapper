6580897ce08871c26f2f6c7331205f7a22f2935a
Naoki Yokoyama <naoki.yokoyama7@gmail.com>
2022-04-20 23:21:10 -0400

    added generate_executables.py

---
+++ generate_executables.py
@@ -0,0 +1,31 @@
+import os
+import os.path as osp
+import sys
+
+this_dir = osp.dirname(osp.abspath(__file__))
+base_dir = osp.join(this_dir, "spot_wrapper")
+bin_dir = osp.join(os.environ["CONDA_PREFIX"], "bin")
+
+orig_to_alias = {
+    "estop": "spot_estop",
+    "headless_estop": "spot_headless_estop",
+    "home_robot": "spot_reset_home",
+    "keyboard_teleop": "spot_keyboard_teleop",
+    "monitor_nav_pose": "spot_monitor_nav_pose",
+    "roll_over": "spot_roll_over",
+    "sit": "spot_sit",
+    "stand": "spot_stand",
+    "view_arm_proprioception": "spot_view_arm_proprioception",
+    "view_camera": "spot_view_camera",
+}
+
+
+print("Generating executables...")
+for orig, alias in orig_to_alias.items():
+    orig_path = osp.join(base_dir, orig + ".py")
+    exe_path = osp.join(bin_dir, alias)
+    data = f"#!/usr/bin/env bash \n{sys.executable} -m spot_wrapper.{orig}\n"
+    with open(exe_path, "w") as f:
+        f.write(data)
+    os.chmod(exe_path, 33277)
+    print("Added:", alias)
