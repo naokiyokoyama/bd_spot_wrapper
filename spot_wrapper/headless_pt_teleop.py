moving silent attr, fixed many bugs

spot_wrapper/headless_pt_teleop.py
@@ -5,6 +5,9 @@ In angular mode, it will turn left or right (respectively).
 Tab key will switch between linear and angular mode.
 If the same key is pressed again while the robot is moving, it will stop.
 """
+import os
+import time
+
 import numpy as np
 from bosdyn.client.estop import EstopClient
 
@@ -15,6 +18,7 @@ from spot_wrapper.utils.headless import KeyboardListener
 UPDATE_PERIOD = 0.2
 LINEAR_VEL = 1.0
 ANGULAR_VEL = np.deg2rad(50)
+DOCK_ID = int(os.environ.get("SPOT_DOCK_ID", 520))
 
 
 class FSM_ID:
@@ -29,35 +33,56 @@ class FSM_ID:
 
 class KEY_ID:
     r"""Keyboard id codes."""
-    UP = "103"
-    DOWN = "108"
-    TAB = "15"
-    ENTER = "28"
+    UP = 103
+    DOWN = 108
+    TAB = 15
+    ENTER = 28
 
 
 class SpotHeadlessTeleop(KeyboardListener):
+    silent = True
+
     def __init__(self):
-        super().__init__()
         self.mode = "linear"
         self.fsm_state = FSM_ID.ESTOP
         self.spot = Spot("HeadlessTeleop")
         self.lease = None
         estop_client = self.spot.robot.ensure_client(EstopClient.default_service_name)
         self.estop_nogui = EstopNoGui(estop_client, 5, "Estop NoGUI")
+        self.last_up = 0
+        self.last_down = 0
+        super().__init__()
 
     def process_pressed_key(self, pressed_key):
         if pressed_key is None:
             return
 
-        if pressed_key == KEY_ID.ENTER and self.fsm_state != FSM_ID.ESTOP:
-            self.estop()
-            self.fsm_state = FSM_ID.ESTOP
-        elif pressed_key == KEY_ID.UP and self.fsm_state == FSM_ID.ESTOP:
-            self.hijack_robot()
+        if pressed_key == KEY_ID.DOWN:
+            self.last_down = time.time()
+        elif pressed_key == KEY_ID.UP:
+            self.last_up = time.time()
+
+        if self.fsm_state == FSM_ID.ESTOP and pressed_key == KEY_ID.UP:
             self.fsm_state = FSM_ID.IDLE
-        elif self.fsm_state == FSM_ID.IDLE:
-            if pressed_key == KEY_ID.TAB:
-                self.mode = "angular" if self.mode == "linear" else "linear"
+            print("Hijacking robot.")
+            self.hijack_robot()
+        elif self.fsm_state != FSM_ID.ESTOP:
+            if pressed_key == KEY_ID.ENTER:
+                self.estop()
+                self.fsm_state = FSM_ID.ESTOP
+                print("E-Stop")
+            elif (
+                pressed_key in [KEY_ID.UP, KEY_ID.DOWN]
+                and abs(self.last_up - self.last_down) < 0.2
+            ):
+                print("Halting and docking robot...")
+                self.last_up = 0
+                self.halt_robot()
+                try:
+                    self.spot.dock(DOCK_ID)
+                    self.spot.home_robot()
+                except:
+                    print("Dock was not found!")
             elif (
                 pressed_key == KEY_ID.UP
                 and self.fsm_state in [FSM_ID.FORWARD, FSM_ID.LEFT]
@@ -67,24 +92,32 @@ class SpotHeadlessTeleop(KeyboardListener):
             ):
                 self.halt_robot()
                 self.fsm_state = FSM_ID.IDLE
+                print("Halting robot.")
+            elif self.fsm_state == FSM_ID.IDLE and pressed_key == KEY_ID.TAB:
+                if self.mode == "linear":
+                    self.mode = "angular"
+                    print("Switching to angular mode.")
+                else:
+                    self.mode = "linear"
+                    print("Switching to linear mode.")
             elif pressed_key == KEY_ID.UP:
                 if self.mode == "linear":
-                    print("Linear up")
                     self.move_forward()
                     self.fsm_state = FSM_ID.FORWARD
+                    print("Moving forwards.")
                 else:
-                    print("Angular left")
                     self.turn_left()
                     self.fsm_state = FSM_ID.LEFT
+                    print("Turning left.")
             elif pressed_key == KEY_ID.DOWN:
                 if self.mode == "linear":
-                    print("Linear down")
                     self.move_backwards()
                     self.fsm_state = FSM_ID.BACK
+                    print("Moving backwards.")
                 else:
-                    print("Angular right")
                     self.turn_right()
                     self.fsm_state = FSM_ID.RIGHT
+                    print("Turning right.")
 
     def estop(self):
         self.estop_nogui.settle_then_cut()
@@ -109,3 +142,7 @@ class SpotHeadlessTeleop(KeyboardListener):
 
     def turn_right(self):
         self.halt_robot(ang_vel=-ANGULAR_VEL)
+
+
+if __name__ == "__main__":
+    SpotHeadlessTeleop()